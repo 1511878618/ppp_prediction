027664deb80c830ccdd5e695bf6d5ce6cf81beab
big_xutingfeng <13984376+big-xutingfeng@user.noreply.gitee.com>
2024-03-24 17:03:10 +0800

    first

---
+++ 3_geneformer_finetune.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@Description:       :
+@Date     :2024/02/19 15:20:56
+@Author      :Tingfeng Xu
+@version      :1.0
+'''
+
+# from .ppp_aging import 
+
+
+import argparse
+
+import textwrap
+import warnings
+import logging
+
+def configure_logger():
+
+    logFormatter = logging.Formatter("[%(levelname)s]  %(message)s")
+    logger = logging.getLogger()
+    logger.setLevel(logging.NOTSET)
+
+
+
+def getParser():
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description=textwrap.dedent(
+            """
+            %prog is ...
+            @Author: xutingfeng@big.ac.cn
+            Version: 1.1
+            
+    
+                ...
+            """
+        ),
+    )
+
+    parser.add_argument("--train", type=str, default=None, help="training data")
+    parser.add_argument("--test", type=str, default=None, help="testing data")
+    parser.add_argument("--model", type=str, default=None, help="model file")
+    parser.add_argument("--output", type=str, default=None, help="output file")
+    parser.add_argument("--epochs", type=int, default=10, help="epochs")
+    parser.add_argument("--batch_size", type=int, default=32, help="batch size")
+
+
+    return parser
+
+from pathlib import Path    
+from geneformer import Classifier
+import datetime
+if __name__ == "__main__":
+    parser = getParser()
+    args = parser.parse_args()
+
+    train_data = args.train
+    test_data = args.test
+    model_directory = args.model
+    output = args.output
+    
+    configure_logger()
+
+
+
+    current_date = datetime.datetime.now()
+    datestamp = f"{str(current_date.year)[-2:]}{current_date.month:02d}{current_date.day:02d}{current_date.hour:02d}{current_date.minute:02d}{current_date.second:02d}"
+    datestamp_min = (
+        f"{str(current_date.year)[-2:]}{current_date.month:02d}{current_date.day:02d}"
+    )
+
+    output_prefix =args.output
+    output_dir = f"./{output_prefix}/{datestamp}"
+    Path(output_dir).mkdir(parents=True, exist_ok=True)
+
+    training_args = {
+        "num_train_epochs": 0.9,
+        "learning_rate": 0.000804,
+        "lr_scheduler_type": "polynomial",
+        "warmup_steps": 1812,
+        "weight_decay": 0.258828,
+        "per_device_train_batch_size": args.batch_size,
+        "per_device_eval_batch_size": args.batch_size,
+        "seed": 73,
+        "epochs": args.epochs,
+        
+    }
+    cc = Classifier(
+        classifier="cell",
+        cell_state_dict={"state_key": "incident_cad", "states": "all"},
+        filter_data=None,
+        training_args=training_args,
+        max_ncells=None,
+        freeze_layers=2,
+        num_crossval_splits=1,
+        forward_batch_size=4,
+        nproc=16,
+    )
+    cc.prepare_data(
+    input_data_file="/home/xutingfeng/ukb/project/ppp_prediction/2_Data/GeneFormer/imputed/train",
+    output_directory=output_dir,
+    output_prefix=output_prefix,
+    # split_id_dict=train_test_id_split_dict,
+    test_size=0.2,
+)
+    # 6 layer Geneformer: https://huggingface.co/ctheodoris/Geneformer/blob/main/model.safetensors
+
+    # model_used = "6_layer"
+    # if model_used == "6_layer":
+    #     model_directory = "/home/xutingfeng/github_code/others/Geneformer/"
+    # elif model_used == "12_layer":
+    #     model_directory = (
+    #         "/home/xutingfeng/github_code/others/Geneformer/geneformer-12L-30M"
+    #     )
+
+
+    all_metrics = cc.validate(
+        model_directory=model_directory,  # 12L
+        prepared_input_data_file=f"{output_dir}/{output_prefix}_labeled_train.dataset",
+        id_class_dict_file=f"{output_dir}/{output_prefix}_id_class_dict.pkl",
+        output_directory=output_dir,
+        output_prefix=output_prefix,
+        split_id_dict=None,
+    )
+    cc = Classifier(classifier="cell",
+                cell_state_dict = {"state_key": "incident_cad", "states": "all"},
+                forward_batch_size=200,
+                nproc=16)
+    
+    all_metrics_test = cc.evaluate_saved_model(
+        model_directory=f"{output_dir}/{datestamp_min}_geneformer_cellClassifier_{output_prefix}/ksplit1/",
+        id_class_dict_file=f"{output_dir}/{output_prefix}_id_class_dict.pkl",
+        test_data_file=f"{output_dir}/{output_prefix}_labeled_test.dataset",
+        output_directory=output_dir,
+        output_prefix=output_prefix,
+    )
+
+
