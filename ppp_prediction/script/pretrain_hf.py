36e616a6761ebcab537b7d82aeb54dff21b71c4b
big_xutingfeng <13984376+big-xutingfeng@user.noreply.gitee.com>
2024-04-20 00:02:11 +0800

    fixbugs and add DL part

--- ppp_prediction/script/pretrain_hf.py
+++ ppp_prediction/script/pretrain_hf.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import argparse
 import textwrap
+from pathlib import Path
 
 
 import numpy as np
@@ -30,8 +31,15 @@ def getParser():
     parser.add_argument("--output", required=True, help="output folder")
     # parser.add_argument("--model", required=True, help="model name")
     parser.add_argument("--tokenizer", required=True, help="tokenizer dir") 
+    parser.add_argument("--batch-size", type=int, default=32, help="batch size")
 
-
+    parser.add_argument(
+        "--precision",
+        type=str,
+        choices=["fp16", "bf16", "tf32"],
+        default="tf32",
+        help="precision",
+    )
 
 
 
@@ -72,14 +80,18 @@ def df2dataset(df, tokenizer, max_length=2048):
 
 
 
-
 if __name__ == "__main__":
     parser = getParser()
     args = parser.parse_args()
 
+    output = args.output
+    Path(output).mkdir(parents=True, exist_ok=True)
+
     train_data = pd.read_pickle(args.train).set_index("eid")
     test_data = pd.read_pickle(args.test).set_index("eid")
     feature_cols = train_data.columns.tolist()
+
+    precision = args.precision
 
     tokenizer = AutoTokenizer.from_pretrained(args.tokenizer)    
     max_length = tokenizer.model_max_length if tokenizer.model_max_length <=1e+5 else 2048
@@ -106,12 +118,17 @@ if __name__ == "__main__":
     tokenizer=tokenizer, mlm_probability=0.15
     )
     training_args = TrainingArguments(
-        output_dir="test",
+        output_dir=output,
         evaluation_strategy="epoch",
         learning_rate=2e-5,
         num_train_epochs=100,
         weight_decay=0.01,
         push_to_hub=False,
+        bf16= (precision == "bf16"),
+        fp16= (precision == "fp16"),
+        per_device_train_batch_size = args.batch_size,
+        per_device_eval_batch_size = args.batch_size,
+        
     )
 
     trainer = Trainer(
