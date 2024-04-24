91c6ef4f10f239c4258bd0a4bd0491caeca8aa08
big_xutingfeng <13984376+big-xutingfeng@user.noreply.gitee.com>
2024-04-24 15:53:22 +0800

    add pretrain for geneformer to proteomics

--- ppp_prediction/constants.py
+++ ppp_prediction/constants.py
@@ -1,4 +1,4 @@
 from pathlib import Path
 GeneFormer_TOKEN_DICTIONARY_FILE = Path(__file__).parent / "geneformer/token_dictionary.pkl"
-GeneFormer__GENE_NAME_ID_DICTIONARY_FILE = Path(__file__).parent / "geneformer/gene_name_id_dict.pkl"
+GeneFormer_GENE_NAME_ID_DICTIONARY_FILE = Path(__file__).parent / "geneformer/gene_name_id_dict.pkl"
 
